5895a864b1cd13c2d188b5f8ec66c4d882437535
Evan <oakeef@gmail.com>
2016-11-23 18:33:18 -0400

    updated the ListFragment and added content
    updated the ListFragment class with a RecyclerView object named
    recyclerView and a ListAdapter object called listAdapter, creative I
    know. The recyclerView sets the listAdapter as it's adapter with the
    setAdapter method, then I created a LinearLayoutManager object called
    layoutManager and set it as the LayoutManager for the recyclerView.
    
    Also I added the animal photos to the drawable folder. Finished creating
    the String[] with the animal names and the int[] with the corresponding
    animnal photos from drawable.
    
    Oh I also changed the theme to dark.

--- app/src/main/java/com/example/evan/assignment3/ImageList.java
+++ app/src/main/java/com/example/evan/assignment3/ImageList.java
@@ -5,8 +5,11 @@ package com.example.evan.assignment3;
  */
 
 public class ImageList {
-    public static String[] names;
-    public static int[] resourceIds;
+    public static String[] names = new String[]{"Bear", "Frog", "Hedgehog", "Lion", "Pig",
+                                                "Red Panda", "Dog"};
+    public static int[] resourceIds = new int[]{R.drawable.bear, R.drawable.frog, R.drawable.hedgehog,
+                                                R.drawable.lion, R.drawable.pig, R.drawable.red_panda,
+                                                R.drawable.dog};
     public static String[] descriptions;
 
 }
--- app/src/main/java/com/example/evan/assignment3/ListFragment.java
+++ app/src/main/java/com/example/evan/assignment3/ListFragment.java
@@ -3,9 +3,13 @@ package com.example.evan.assignment3;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import static com.example.evan.assignment3.R.id.listRecyclerView;
 
 /**
  * Created by Evan on 11/21/2016.
@@ -19,6 +23,15 @@ public class ListFragment extends Fragment{
 
         View view = inflater.inflate(R.layout.fragment_list, container, false);
 
+        RecyclerView recyclerView = (RecyclerView) view.findViewById(listRecyclerView);
+
+        ListAdapter listAdapter = new ListAdapter();
+
+        recyclerView.setAdapter(listAdapter);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+
+        recyclerView.setLayoutManager(layoutManager);
         return view;
     }
 }
